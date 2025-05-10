c5f18ee829ac57716e0ea9683b2c0824eb747527
dotpy88 <tsmtbs07@gmail.com>
2025-05-10 09:05:56 -0500

    Added first app and test case

---
+++ mayapp/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class TestCalls(TestCase):
+
+    def test_call_view_load(self):
+        response = self.client.get('/mayapp/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Hello, world. You're at the mayapp index.")
